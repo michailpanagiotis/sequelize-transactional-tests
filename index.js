50210b74523f88b01f573b10a2129d2411cc0ccb
Panos Michail <michailpanagiotis@gmail.com>
2020-09-05 23:06:54 +0000

    WIP

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const Mocha = require('mocha');
 const Sequelize = require('sequelize');
-const EventEmitter = require('events');
 const cls = require('cls-hooked');
 const { Transaction } = require('sequelize');
-
-let tHandler;
 
 const noClsError = new Error(`
   Sequelize does not have a CLS, set it by running Sequelize.useCLS(namespace)
@@ -16,11 +13,11 @@ const getNamespace = () => {
     throw noClsError;
   }
   const { name } = Sequelize._cls;
-  const namespace = cls.getNamespace(name);
-  return namespace;
+  return cls.getNamespace(name);
 };
 
 const getCurrentTransaction = () => getNamespace().get('transaction');
+const setCurrentTransaction = (tr) => getNamespace().set('transaction', tr);
 
 const getLastSavepoint = () => {
   const tr = getCurrentTransaction();
@@ -39,109 +36,22 @@ const getLastSavepoint = () => {
   return lastSp;
 };
 
-class TransactionHandler extends EventEmitter {
-  constructor({
-    sequelize, commitOnError = false, wrapRoot = false, wrapChildren = false,
-    onTransaction = () => {}, onCommit = () => {}, onRollback = () => {},
-  } = {}) {
-    if (!sequelize) {
-      throw new Error('undefined parameter \'sequelize\'');
-    }
-    super();
-    this.sequelize = sequelize;
-    this.commitOnError = commitOnError;
-    this.wrapChildren = wrapChildren;
-    this.wrapRoot = wrapRoot;
-    this.onTransaction = onTransaction;
-    this.onCommit = onCommit;
-    this.onRollback = onRollback;
-  }
-
-  getSequelize() {
-    return typeof this.sequelize === 'function' ? this.sequelize() : this.sequelize;
-  }
-
-  transaction(mochaPath) {
-    const sequelize = this.getSequelize();
-    const current = getCurrentTransaction();
-    return sequelize.transaction({
-      transaction: current,
-      isolationLevel: Transaction.ISOLATION_LEVELS.READ_UNCOMMITTED,
-    }).then((t) => {
-      if (!current) {
-        this._patchSavepoints(t);
-        getNamespace().set('transaction', t);
-      }
-      this.emit('transaction', t, mochaPath);
-      return t;
-    }).then((t) => this.onTransaction(t, mochaPath));
-  }
-
-  stop(failed = false, mochaPath) {
-    const { commitOnError } = this;
-    return failed && commitOnError ? this._commit(mochaPath) : this._rollback(mochaPath);
-  }
-
-  _patchSequelize() {
-    const sequelize = this.getSequelize();
-    const transactionFn = sequelize.transaction;
-    Object.assign(sequelize, {
-      transaction: function transaction(...args) {
-        process.emitWarning('user transactions ', 'TransactionalWarning');
-        return transactionFn.bind(this)(...args);
-      },
-    });
-  }
-
-  _commit(mochaPath) {
-    const ended = getLastSavepoint();
-    if (!ended || ended.finished) {
-      return Promise.resolve(ended);
-    }
-    return ended.commit().then(() => {
-      if (!ended.parent) {
-        getNamespace().set('transaction', null);
-      }
-      this.emit('commit', ended, mochaPath);
-      return ended;
-    }).then((t) => this.onCommit(t, mochaPath));
-  }
-
-  _rollback(mochaPath) {
-    const ended = getLastSavepoint();
-    if (!ended) {
-      throw new Error('missing ended');
-    }
-    if (!ended || ended.finished) {
-      return Promise.resolve(ended);
-    }
-    return ended.rollback().then(() => {
-      if (!ended.parent) {
-        getNamespace().set('transaction', null);
-      }
-      this.emit('rollback', ended, mochaPath);
-      return ended;
-    }).then((t) => this.onRollback(t, mochaPath));
-  }
-
-  _patchSavepoints(transaction) {
-    const { commitOnError } = this;
-    Object.assign(transaction.savepoints, {
-      push: function push(savepoint) {
-        const origCommit = savepoint.commit.bind(savepoint);
-        Object.assign(savepoint, {
-          commit: () => {
-            if (!commitOnError) {
-              process.emitWarning('transaction commits are disabled in order for tests to use the transactional pattern', 'TransactionalWarning');
-            }
-            return origCommit();
-          },
-        });
-        Array.prototype.push.call(this, savepoint);
-      },
-    });
-  }
-}
+const patchSavepoints = (transaction, { commitOnError = false } = {}) => {
+  Object.assign(transaction.savepoints, {
+    push: function push(savepoint) {
+      const origCommit = savepoint.commit.bind(savepoint);
+      Object.assign(savepoint, {
+        commit: () => {
+          if (!commitOnError) {
+            process.emitWarning('transaction commits are disabled in order for tests to use the transactional pattern', 'TransactionalWarning');
+          }
+          return origCommit();
+        },
+      });
+      Array.prototype.push.call(this, savepoint);
+    },
+  });
+};
 
 const walkSuite = (suite, fn) => {
   fn(suite);
@@ -159,38 +69,102 @@ const suiteFailed = (suite) => {
   return failed;
 };
 
-const bindToNamespace = (suite) => {
-  const namespace = getNamespace();
+const bindHooksAndTests = (suite) => {
   ['tests', '_beforeAll', '_beforeEach', '_afterAll', '_afterEach'].forEach((set) => {
     suite[set].forEach((test) => {
-      Object.assign(test, { fn: namespace.bind(test.fn) });
+      Object.assign(test, { fn: getNamespace().bind(test.fn) });
     });
   });
 };
 
-const makeTransactional = (rootSuite) => {
-  if (typeof rootSuite === 'function') {
-    return function wT(...args) {
-      const res = rootSuite.bind(this)(...args);
-      makeTransactional(this);
-      return res;
-    };
-  }
-  const namespace = getNamespace();
-  return namespace.bind(walkSuite)(rootSuite, (suite) => {
-    if (suite === rootSuite) {
-      // add mocha hooks to tests
-      suite.beforeEach(function beforeEach() {
-        const inPath = this.currentTest.fullTitle();
-        return tHandler.transaction(inPath);
-      });
-      // make sure the transaction starts before any other beforeEach hook
-      suite._beforeEach.unshift(suite._beforeEach.pop());
-      suite.afterEach(function afterEach() {
-        const inPath = this.currentTest.fullTitle();
-        return tHandler.stop(suiteFailed(suite), inPath);
-      });
+class SequelizeTransactionHandler {
+  constructor({
+    sequelize, commitOnError = false,
+    onTransaction = () => {}, onCommit = () => {}, onRollback = () => {},
+  } = {}) {
+    if (!sequelize) {
+      throw new Error('undefined parameter \'sequelize\'');
     }
+    this.sequelize = sequelize;
+    this.commitOnError = commitOnError;
+    this.onTransaction = onTransaction;
+    this.onCommit = onCommit;
+    this.onRollback = onRollback;
+  }
+
+  transaction(descriptor) {
+    const sequelize = typeof this.sequelize === 'function' ? this.sequelize() : this.sequelize;
+    const current = getCurrentTransaction();
+    return sequelize.transaction({
+      transaction: current,
+      isolationLevel: Transaction.ISOLATION_LEVELS.READ_UNCOMMITTED,
+    }).then((t) => {
+      if (!current) {
+        patchSavepoints(t);
+        setCurrentTransaction(t);
+      }
+      return t;
+    }).then((t) => this.onTransaction(t, descriptor));
+  }
+
+  stop(failed = false, descriptor) {
+    const { commitOnError } = this;
+    return failed && commitOnError ? this.commit(descriptor) : this.rollback(descriptor);
+  }
+
+  commit(descriptor) {
+    const ended = getLastSavepoint();
+    if (!ended || ended.finished) {
+      return Promise.resolve(ended);
+    }
+    return ended.commit().then(() => {
+      if (!ended.parent) {
+        setCurrentTransaction(null);
+      }
+      return ended;
+    }).then((t) => this.onCommit(t, descriptor));
+  }
+
+  rollback(descriptor) {
+    const ended = getLastSavepoint();
+    if (!ended) {
+      throw new Error('missing ended');
+    }
+    if (!ended || ended.finished) {
+      return Promise.resolve(ended);
+    }
+    return ended.rollback().then(() => {
+      if (!ended.parent) {
+        setCurrentTransaction(null);
+      }
+      return ended;
+    }).then((t) => this.onRollback(t, descriptor));
+  }
+}
+
+class MochaSuiteHandler {
+  constructor(transactionHandler, { wrapChildren = false } = {}) {
+    this.tHandler = transactionHandler;
+    this.wrapChildren = wrapChildren;
+  }
+
+  wrapTests(suite) {
+    const { tHandler } = this;
+    // add mocha hooks to tests
+    suite.beforeEach(function beforeEach() {
+      const inPath = this.currentTest.fullTitle();
+      return tHandler.transaction(inPath);
+    });
+    // make sure the transaction starts before any other beforeEach hook
+    suite._beforeEach.unshift(suite._beforeEach.pop());
+    suite.afterEach(function afterEach() {
+      const inPath = this.currentTest.fullTitle();
+      return tHandler.stop(suiteFailed(suite), inPath);
+    });
+  }
+
+  wrapSuite(suite) {
+    const { tHandler, wrapChildren } = this;
     if (!suite.root) {
       // add mocha hooks to inner suites
       const path = suite.fullTitle();
@@ -199,7 +173,7 @@ const makeTransactional = (rootSuite) => {
       // make sure the transaction starts before any other beforeAll hook
       suite._beforeAll.unshift(suite._beforeAll.pop());
 
-      if (tHandler.wrapChildren) {
+      if (wrapChildren) {
         suite.beforeAll(() => tHandler.transaction(inPath));
 
         suite.afterAll(() => tHandler.stop(suiteFailed(suite), inPath));
@@ -209,33 +183,58 @@ const makeTransactional = (rootSuite) => {
 
       suite.afterAll(() => tHandler.stop(suiteFailed(suite), path));
     }
-    bindToNamespace(suite);
-  });
-};
+    bindHooksAndTests(suite);
+  }
+}
 
-const patchRunner = () => {
-  const runnerFn = Mocha.Runner.prototype.run;
-  Object.assign(Mocha.Runner.prototype, {
-    run: function run(...args) {
-      if (tHandler.wrapRoot) {
-        // add mocha hooks for starting/rolling back transactions on the
-        // boundaries of each test/suite
-        makeTransactional(this.suite);
+class Transactional {
+  constructor({
+    sequelize, commitOnError = false, wrapChildren = false, wrapRoot = false,
+    onTransaction = () => {}, onCommit = () => {}, onRollback = () => {},
+  } = {}) {
+    if (!sequelize) {
+      throw new Error('undefined parameter \'sequelize\'');
+    }
+    const transactionHandler = new SequelizeTransactionHandler({
+      sequelize, commitOnError, onTransaction, onCommit, onRollback,
+    });
+    this.suiteHandler = new MochaSuiteHandler(transactionHandler, { wrapChildren });
+    if (wrapRoot) {
+      try {
+        const runnerFn = Mocha.Runner.prototype.runSuite;
+        const tHandler = this;
+        Object.assign(Mocha.Runner.prototype, {
+          runSuite: function run(suite, fn) {
+            if (suite.root) {
+              // add mocha hooks for starting/rolling back transactions on the
+              // boundaries of each test/suite
+              tHandler.bind(suite);
+            }
+            return runnerFn.bind(this)(suite, fn);
+          },
+        });
+      } catch (e) {
+        console.error(e);
       }
-      // bind the Mocha runner in the current CLS namespace of sequelize in order for transactions
-      // to be automatically applied to each sequelize operation
-      return runnerFn.bind(this)(...args);
-    },
-  });
-};
+    }
+  }
 
-const patchMocha = (...args) => {
-  tHandler = new TransactionHandler(...args);
-  patchRunner();
-  return tHandler;
-};
+  bind(suite) {
+    const transactional = this;
+    const { suiteHandler } = this;
+    if (typeof suite === 'function') {
+      return function wT(...args) {
+        const res = suite.bind(this)(...args);
+        transactional.bind(this);
+        return res;
+      };
+    }
 
-module.exports = {
-  makeTransactional,
-  patchMocha,
-};
+    return getNamespace().run(() => {
+      suiteHandler.wrapTests(suite);
+      walkSuite(suite, suiteHandler.wrapSuite.bind(suiteHandler));
+    });
+  }
+}
+
+module.exports = Transactional;
